9e22e3457ed17fdb96fbfa4eb490704710307d9b
Monkey D. Hyuck <kickscar@gmail.com>
2023-09-05 20:20:41 +0900

    찡!! 일요일 작업 commentary & naggings

--- mypofol01/controllers/account.js
+++ mypofol01/controllers/account.js
@@ -28,10 +28,10 @@ module.exports = {
          *  아 참고로...
          *  /bella 여기에 다른 로그인 한 사용자가 접근할 수 있쟎어?
          *  로그인한 사용자랑 헷갈릴 수 있어
-         *  /bella 의 접근하는 계정 정보는 account라 했음!!!
+         *  /bella 저급 계정의 정보는 account라 했음!!!
          * 
-         *  디비가 바껴서 user와 profile은 분리 했음
-         *  헷갈리니깐... 꼭 참고해서 바라~~
+         *  디비가 바껴서 user와 profile은 분리됐다.
+         *  꼭 참고해서 바라~~ data-schema-mypofol 폴더 확인해라
          * 
          */
         
@@ -59,9 +59,11 @@ module.exports = {
           }, {})
         });
       } catch (error) {
-        /** 
-         * 빼먹지 말고!!!
-         * 에러나면 응답이 안와.... 계속 브라우저 빙글빙글 돌아~
+        /**
+         *  
+         * 빼먹지 말고 꼭 next(err) 그래야 500 에러 페이지 나온다.
+         * 에러나면 응답이 안와.... 계속 브라우저 빙글빙글 돈다.
+         * 
          */
         next(err);
       }
@@ -69,18 +71,18 @@ module.exports = {
     },
     projects: async (req, res, next) => {
       try {
+
         /**
          *  여기도 위와 마찬가지!!!
-         * 
-         *  인터셉터 많이 쓰는 기능이니깐 꼭 이해 해주면 좋겠음~~
+         *  인터셉터는 많이 쓰는 기능이니깐 꼭 이해 해줘라
          * 
          */
+
         // const profile = await modelUser.findByAccount(req.params.account);
         // if(!profile){
         //  res.status(404).render('error/404');
         //  return;
         // }
-
         // const projects = await modelProject.findByUserId(req.account.id);
         // res.status(200).render('account/projects', {
         //   account: req.account,
