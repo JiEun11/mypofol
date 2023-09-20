e57ca3eccaf4709735f821f8eb9d52a7c32420d5
Monkey D. Hyuck <kickscar@gmail.com>
2023-09-20 21:11:15 +0900

    [CODE] Router/Main 수정

--- mypofol02/frontend/src/layout/Header.js
+++ mypofol02/frontend/src/layout/Header.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink} from 'react-router-dom';
 import '../assets/css/layout/Header.css';
 
 const Header = ({a, openDialog}) => {
     return (
-        <header className="header">
-            <div className="globalbar">
-                <div className="inner-start">
-                    <a href="/" className="app-logo">MyPofol</a>
+        <header className='header'>
+            <div className='globalbar'>
+                <div className='inner-start'>
+                    <a href='/' className='app-logo'>MyPofol</a>
                     {   !a ? 
-                        <div className="header-context">
+                        <div className='header-context'>
                             <ul>
                                 <li>
-                                    <a href="/bella" className="context-item">bella</a>
+                                    <a href='/bella' className='context-item'>bella</a>
                                 </li>
                             </ul>
                         </div> :
                         null
                     }
                 </div>
-                <div className="inner-end">
+                <div className='inner-end'>
                     {
                         a ?
                         <> 
-                            <div className="header-context">
+                            <div className='header-context'>
                             <ul>
                                 <li>
-                                    <label className="context-item">
-                                        <NavLink to={"/signin"}>로그인</NavLink>
+                                    <label className='context-item'>
+                                        <NavLink to={'/signin'}>로그인</NavLink>
                                     </label>
                                 </li>            
                                 <li>
-                                    <label className="context-item">
-                                        <NavLink to={"/signup"}>회원가입</NavLink>
+                                    <label className='context-item'>
+                                        <NavLink to={'/signup'}>회원가입</NavLink>
                                     </label>
                                 </li>
                             </ul>
@@ -40,13 +40,13 @@ const Header = ({a, openDialog}) => {
                         </>
                         :
                         <>    
-                            <a className="header-button has-indicator">
-                                <svg viewBox="0 0 16 16" className="inbox">
-                                    <path d="M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z"></path>
+                            <a className='header-button has-indicator'>
+                                <svg viewBox='0 0 16 16' className='inbox'>
+                                    <path d='M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z'></path>
                                 </svg>
                             </a>
-                            <label className="header-user">
-                                <img src="/images/default_profile.png" />
+                            <label className='header-user'>
+                                <img src='/images/default_profile.png' />
                             </label>
                         </>
                     }                  
--- mypofol02/frontend/src/layout/SidebarAccount.js
+++ mypofol02/frontend/src/layout/SidebarAccount.js
@@ -1,73 +1,73 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import {NavLink} from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import '../assets/css/layout/SidebarAccount.css';
 
 const SidebarAccount = () => {
   const { accountName } = useParams();
 
   return (
-    <div className="sidebar">
-      <div className="sidebar-header">
-        <div className="profile-pic">
-          <a href="#">
-            <img src="/images/default_profile.png" />
+    <div className='sidebar'>
+      <div className='sidebar-header'>
+        <div className='profile-pic'>
+          <a href='#'>
+            <img src='/images/default_profile.png' />
           </a>
         </div>
-        <h1 className="vcard-names">
-          <span className="name">bella</span>
-          <span className="title">developer</span>
+        <h1 className='vcard-names'>
+          <span className='name'>bella</span>
+          <span className='title'>developer</span>
         </h1>
       </div>
-      <div className="sidebar-content">
-        <nav className="navbar">
+      <div className='sidebar-content'>
+        <nav className='navbar'>
           <ul>
             <li>
               <NavLink to={`/${accountName}/profile`}>
-                <span className="item-ico">
-                  <i className="fa fa-address-card"></i>
+                <span className='item-ico'>
+                  <i className='fa fa-address-card'></i>
                 </span>
-                <span className="item-label">프로필</span>           
+                <span className='item-label'>프로필</span>           
               </NavLink>
             </li>
             <li>
-              <a href="/bella/experiences">
-                <span className="item-ico">
-                  <i className="fa fa-list-ul"></i>
+              <a href='/bella/experiences'>
+                <span className='item-ico'>
+                  <i className='fa fa-list-ul'></i>
                 </span>
-                <span className="item-label">경력</span>
+                <span className='item-label'>경력</span>
               </a>
             </li>
             <li>
               <NavLink to={`/${accountName}/educations`}>
-                <span className="item-ico">
-                  <i className="fa fa-graduation-cap"></i>
+                <span className='item-ico'>
+                  <i className='fa fa-graduation-cap'></i>
                 </span>
-                <span className="item-label">학력</span>
+                <span className='item-label'>학력</span>
               </NavLink>
             </li>
             <li>
-              <a href="/bella/trainings">
-                <span className="item-ico">
-                  <i className="fa fa-award"></i>
+              <a href='/bella/trainings'>
+                <span className='item-ico'>
+                  <i className='fa fa-award'></i>
                 </span>
-                <span className="item-label">교육 및 자격증</span>
+                <span className='item-label'>교육 및 자격증</span>
               </a>
             </li>
             <li>
-              <a href="/bella/skills">
-                <span className="item-ico">
-                  <i className="fa fa-code"></i>
+              <a href='/bella/skills'>
+                <span className='item-ico'>
+                  <i className='fa fa-code'></i>
                 </span>
-                <span className="item-label">주요기술</span>
+                <span className='item-label'>주요기술</span>
               </a>
             </li>
             <li>
-              <a href="/bella/projects">
-                <span className="item-ico">
-                  <i className="fa fa-diagram-project"></i>
+              <a href='/bella/projects'>
+                <span className='item-ico'>
+                  <i className='fa fa-diagram-project'></i>
                 </span>
-                <span className="item-label">프로젝트</span>
+                <span className='item-label'>프로젝트</span>
               </a>
             </li>
           </ul>
