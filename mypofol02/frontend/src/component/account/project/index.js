c843ca3062d476d27a14d8d4b6100a414a9b253b
Bella <shdudtnr3939@naver.com>
2023-09-23 14:20:31 +0900

    [CODE] className으로 변경

--- mypofol02/frontend/src/component/account/project/index.js
+++ mypofol02/frontend/src/component/account/project/index.js
@@ -6,14 +6,14 @@ import '../../../assets/css/component/account/Projects.css';
 const index = () => {
   return (
     <LayoutAccount>
-      <div class="projects">
-                <div class="content-inner">
-                    <div class="content-header">
+      <div className="projects">
+                <div className="content-inner">
+                    <div className="content-header">
                         <h2>프로젝트</h2>
                     </div>
-                    <div class="row align-items-center">
-                        <div class="col-md-12">
-                            <div class="prj-col">
+                    <div className="row align-items-center">
+                        <div className="col-md-12">
+                            <div className="prj-col">
                                 <span>2023.10<i>~</i> 2023.12</span>
                                 <h3>Mypofol</h3>
                                 <h4>Project Manager</h4>
