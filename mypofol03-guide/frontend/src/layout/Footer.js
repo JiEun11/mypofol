00d5974bdb5ad9603c6fa05cc79ad4df181bfd63
Monkey D. Hyuck <kickscar@gmail.com>
2023-09-28 13:12:09 +0900

    Footer copyright escape fix

--- mypofol03-guide/frontend/src/layout/Footer.js
+++ mypofol03-guide/frontend/src/layout/Footer.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 import '../assets/css/layout/Footer.css';
 
 const Footer = () => {
   return (
-    <div className={"footer"}>
-      <div className={"content-inner"}>
-        <div className={"row align-items-center"}>
-          <div className={"col-md-6"}>
+    <div className={'footer'}>
+      <div className={'content-inner'}>
+        <div className={'row align-items-center'}>
+          <div className={'col-md-6'}>
             <p>
-              {"&copy; Copyright 2023 "}
-              <a href="https://github.com/JiEun11">Bella Jin</a>
-              {"All Rights Reserved"}
+              &copy;{' Copyright 2023 '}
+              <a href='https://github.com/JiEun11'>Bella Jin</a>
+              {'All Rights Reserved'}
             </p>
           </div>
-          <div className="col-md-6">
+          <div className='col-md-6'>
             <p>
-              {"Powered by "}
-              <a href="https://github.com/JiEun11">Bella Jin</a>
+              {'Powered by '}
+              <a href='https://github.com/JiEun11'>Bella Jin</a>
             </p>
           </div>
         </div>
