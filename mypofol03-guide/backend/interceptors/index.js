934dd31ed62171a35e3ccb5d644e65eadb8148db
Monkey D. Hyuck <kickscar@gmail.com>
2023-10-02 14:35:41 +0900

    [CODE] interceptor 정리

---
+++ mypofol03-guide/backend/interceptors/index.js
@@ -0,0 +1,5 @@
+const { verifyToken } = require('./jwt');
+const { validAccount } = require('./security');
+const { delayForTest } = require('./tools');
+
+module.exports = { verifyToken, validAccount, delayForTest }
